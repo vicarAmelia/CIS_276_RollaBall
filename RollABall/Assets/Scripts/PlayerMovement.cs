1d6f94a763e6f7b2811bbfc41cc3169554547231
vicarAmelia <98558073+vicarAmelia@users.noreply.github.com>
2022-02-02 11:49:17 -0500

    Updated player movement script

--- RollABall/Assets/Scripts/PlayerMovement.cs
+++ RollABall/Assets/Scripts/PlayerMovement.cs
@@ -2,17 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+        private Vector3 moveInput;
+        public Rigidbody rb;
+        private PlayerCamera playerCam;
+        private float speed = 4f;
+        private float jumpForce = 3f;
+
+        private void Awake()
+        {
+            rb = GetComponent<Rigidbody>();
+            playerCam = FindObjectOfType<PlayerCamera>();
+        }
+}
+
+private void FixedUpdate()
+{
+   MovePlayer(); 
+}
+
+private void Update()
+{
+    moveInput = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"))
+
+    if(Input.GetKeyDown(KeyCode.Space))
     {
-        
+        rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse); 
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+private void MovePlayer()
+{
+    rb.velocity = new Vector3(moveInput.x * speed, rb.velocity.y, moveInput.z * speed);
+}
 }
